bd58137b64ff30b9cf180ab20d3315a537bc1f4d
djeff971 <kyuside@gmail.com>
2021-07-03 21:26:38 +0200

    4 files modified, 49 added - OK auth

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+
+import { HomeComponent } from "./home";
+import { AuthGuard } from "./_helpers";
+const accountModule = () =>
+  import("./account/account.module").then(x => x.AccountModule);
+const usersModule = () =>
+  import("./users/users.module").then(x => x.UsersModule);
+const partenairesModule = () =>
+  import("./partenaires/partenaires.module").then(x => x.PartenairesModule);
+
+const routes: Routes = [
+  { path: "", component: HomeComponent, canActivate: [AuthGuard] },
+  { path: "users", loadChildren: usersModule, canActivate: [AuthGuard] },
+  { path: "account", loadChildren: accountModule },
+  { path: "p", loadChildren: partenairesModule },
+
+  // otherwise redirect to home
+  { path: "**", redirectTo: "" }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,34 @@
+import { Component, Input, Output, EventEmitter } from "@angular/core";
+
+import { User } from "../_models";
+import { AccountService } from "../_services";
+import { ProductsModel } from "../_models/products";
+import { StorageService } from "../_services/storage.service";
+import { CartService } from "../_services/cart.service";
+
+@Component({ templateUrl: "home.component.html" })
+export class HomeComponent {
+  user: User;
+  public cartflag: boolean = false;
+  public sortBy: string = "";
+  public sortOption: string = "product_name|asc";
+
+  constructor(
+    private accountService: AccountService,
+    public products: ProductsModel
+  ) {
+    this.user = this.accountService.userValue;
+  }
+
+  ngOnInit() {
+    this.ref();
+  }
+
+  // Fonction ajoutée pour le panier
+  ref() {
+    this.cartflag = false;
+    setTimeout(() => {
+      this.cartflag = true;
+    }, 10);
+  }
+}
